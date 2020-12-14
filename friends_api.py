38aa383592a88505beecde678db945194d1d07d7
WannaFight <itrushin99@gmail.com>
2020-12-14 15:22:02 +0300

    add favicon

--- friends_api.py
+++ friends_api.py
@@ -1,4 +1,6 @@
-from flask import Flask, jsonify, request
+from os import path
+
+from flask import Flask, jsonify, send_from_directory
 
 from parser import get_friends_cities
 
@@ -11,9 +13,15 @@ def index():
     return "Welcome to my api!\n"
 
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(path.join(app.root_path, 'static'), 'vk.ico',
+                               mimetype='image/vnd.microsoft.icon')
+
+
 @app.route('/cities/<string:target_id>')
 def get_friends_stats(target_id):
-    return jsonify(get_friends_cities(target_id))
+    return get_friends_cities(target_id)
 
 
 if __name__ == "__main__":
